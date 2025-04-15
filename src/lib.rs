efe9936854e747612859c03e8420114c64f6f121
Yanna <yannajask@gmail.com>
2025-04-14 20:42:01 -0400

    change to sequential access of matrix data

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ use std::cmp::min;
 
 #[derive(Debug, Clone)]
 pub struct Matrix {
-    pub data: Vec<Vec<f64>>,
+    pub data: Vec<f64>,
     pub rows: usize,
     pub cols: usize,
 }
@@ -12,7 +12,7 @@ impl Matrix {
     #[inline]
     pub fn new(m: usize, n: usize) -> Matrix {
         Matrix {
-            data: vec![vec![0.0; n]; m],
+            data: vec![0.0; m * n],
             rows: m,
             cols: n,
         }
@@ -23,15 +23,15 @@ impl Matrix {
     pub fn identity(m: usize, n: usize) -> Matrix {
         let mut identity_matrix = Matrix::new(m, n);
         for i in 0..std::cmp::min(m, n) {
-            identity_matrix.data[i][i] = 1.0;
+            identity_matrix.data[i * n + i] = 1.0;
         }
         identity_matrix
     }
 
-    /// Returns the a_ij element of a matrix.
+    /// Returns the `a_ij`-th element of a matrix.
     #[inline]
     fn get(&self, i: usize, j: usize) -> f64 {
-        return self.data[i][j];
+        return self.data[i * self.cols + j];
     }
 
     /// Returns the transpose of a matrix.
@@ -40,11 +40,32 @@ impl Matrix {
         let mut transposed = Matrix::new(self.cols, self.rows);
         for i in 0..self.rows {
             for j in 0..self.cols {
-                transposed.data[j][i] = self.data[i][j];
+                transposed.data[j * self.rows + i] = self.data[i * self.cols + j];
             }
         }
         transposed
     }
+}
+
+fn sign(x: f64) -> f64 {
+    if x > 0.0 {
+        1.0
+    } else {
+        -1.0
+    }
+}
+
+fn norm(x: &[f64]) -> f64 {
+    let mut norm = 0.0_f64;
+    for i in x {
+        norm += i * i;
+    }
+    norm.sqrt()
+}
+
+fn normalize(x: &[f64]) -> Vec<f64> {
+    let norm = norm(x);
+    x.iter().map(|&x| x / norm).collect()
 }
 
 /// Returns the matrix product of A and B.
@@ -53,16 +74,55 @@ impl Matrix {
 #[inline]
 pub fn matrix_multiply(a: &Matrix, b: &Matrix) -> Matrix {
     assert_eq!(a.cols, b.rows, "A must have the same number of columns as the number of rows in B.");
-    let b_prime = b.transpose();
     let mut product = Matrix::new(a.rows, b.cols);
     for i in 0..a.rows {
-        for j in 0..b_prime.rows {
-            let mut sum: f64 = 0.0;
+        for j in 0..b.cols {
+            let mut sum = 0.0;
             for k in 0..a.cols {
-                sum += a.get(i, j) * b_prime.get(j, k);
+                sum += a.get(i, j) * b.get(k, j);
             }
-            product.data[i][j] = sum;
+            product.data[i + b.cols + j] = sum;
         }
     }
     product
+}
+
+//https://www.math.iit.edu/~fass/477577_Chapter_12.pdf
+pub fn householder_bidiag(a: &Matrix) -> (Matrix, Matrix, Matrix) {
+    let m = a.rows;
+    let n = a.cols;
+    let mut b = a.clone();
+    let mut v = Matrix::identity(m, m);
+    let mut u_t = Matrix::identity(n, n);
+
+    for k in 0..n {
+        // x = A(k: m,k)
+        // u_k = x + sign(x[0]) * x.norm() * e_1
+        // u_k = u_k.norm()
+        let x = &b.data[k..k+m]; // this is wrong, we need the column vector
+        let mut u_k = x.to_vec();
+        u_k[0] += sign(x[0]) * norm(x);
+        u_k = normalize(&u_k);
+        u_t.data[k*n..(k+1)*n].copy_from_slice(&u_k);
+        // B(k: m, k: n) -= 2 * u_k (u_k^t * B[k:m, k:n])
+
+        if k <= (n - 2) {
+            let x = &b.data[(k*n)+k+1..(k*n)+n];
+            let mut v_k = x.to_vec();
+            v_k[0] += sign(x[0]) * norm(x);
+            v_k = normalize(&v_k);
+            // goes to column vector in v
+            // B[k:m, (k+1):n] -= 2 * A[k:m, (k+1):n, v_k] & v_k^t
+        }
+    }
+
+    for i in 0..a.cols {
+        // v_k = sign(x) * x.abs() * e_1 + x
+        // v_k = normalize(v_k)
+        for j in i..a.cols {
+            // A(k: m, j) = A(k: m, j) - (2 * v_k * (v_k_t * A(k: m, j))
+        }
+    }
+
+    (v, b, u_t)
 }
